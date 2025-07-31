59ef702511601b2278e1141e9afa40e5515b6562
MarcoFele98 <marcofelemarco@gmail.com>
2025-07-31 13:14:33 +0100

    update

--- Code/R/example_logistic_growth.R
+++ Code/R/example_logistic_growth.R
@@ -15,7 +15,7 @@ options(scipen = 10)
 sourceCpp("gillespie_algorithm.cpp")
 
 
-# Example logistic regression ----
+# Example logistic growth ----
 
 # Initialize simulations
 runs <- gillespie_simulation(states = c(10),
@@ -71,6 +71,23 @@ for(rho in seq(2, 3, by = 0.25)) {
 (p <- ggplot(runs,
              aes(time, species_0, 
                  color = rho)) +
+    geom_step(aes(group = interaction(replicate, rho)),
+              alpha = 0.2) +
+    geom_smooth(aes(group = rho),
+                se = F) +
+    xlim(c(0, 2)) +
+    scale_color_viridis_c(name = "\u03C1") +
+    xlab("Time") +
+    ylab("Number"))
+
+ggsave("../../figures/logistic_growth_reproduction.png",
+       height = 3,
+       width = 5,
+       bg = "white")
+
+(p <- ggplot(runs,
+             aes(time, species_0, 
+                 color = rho)) +
   geom_step(aes(group = interaction(replicate, rho)),
             alpha = 0.2) +
     geom_smooth(aes(group = rho),
@@ -94,17 +111,23 @@ voter_ode <- function(time, state, parms) {
 }
 
 # Solve the ODE
-out <- deSolve::ode(y = c(y = 0.01), 
-                    times = seq(0, 500, by = 1), 
-                    func = voter_ode, 
-                    parms = c(alpha_1 = 1, 
-                              alpha_2 = 1, 
-                              sigma_1 = 0.01, 
-                              sigma_2 = 0.01))
+out <- data.table(time = numeric(), y = numeric(), start = numeric())
+for(start in seq(0, 1, by = 0.2)) {
+  out <- out |>
+    rbind((deSolve::ode(y = c(y = start), 
+                        times = seq(0, 500, by = 1), 
+                        func = voter_ode, 
+                        parms = c(alpha_1 = 1, 
+                                  alpha_2 = 1, 
+                                  sigma_1 = 0.01, 
+                                  sigma_2 = 0.01)) |>
+             as.data.table()
+    )[, ":="(start = start)])
+}
 
 # Plot and save the result
 (p <- ggplot(out) +
-  geom_line(aes(time, y), linewidth = 1) +
+  geom_line(aes(time, y, group = start), linewidth = 1) +
   ylab("Proportion prefering option A") +
   ylim(c(0, 1)) +
   xlab("Time"))
@@ -168,13 +191,12 @@ for(replicate in 2:50) {
             cbind(replicate = replicate) |>
             as.data.table()
     )[, ":="(duration = c(tail(waiting_time, -1), NA))])
-  
 }
 
 # Plot and save results
 (p <- ggplot(data_l[!is.na(duration) & time > 500]) +
   geom_histogram(aes(x = species_0, weight = duration, y = after_stat(density)),
-                 bins = 51, fill = "black", color = "black", alpha = 0.1) +
+                 bins = 51, fill = "black", color = NA, alpha = 0.3) +
   xlab("Number prefering option A") +
   ylab("Probability"))
 
@@ -187,9 +209,12 @@ ggsave("../../figures/voter_model_stochastic.png",
 # Non-linear opinion-dynamics model ----
 
 # Long term behaviour
-growth_decrese_nod <- data.table(x = seq(-2, 2, by = 0.01),
+growth_decrese_nod_1 <- data.table(x = seq(-2, 2, by = 0.01),
                                decrease_rate = seq(-2, 2, by = 0.01),
-                               growth_rate = tanh(10 * seq(-2, 2, by = 0.01))) |>
+                               growth_rate = tanh(10 * seq(-2, 2, by = 0.01)),
+                               jacobian = -1 + 10 * pracma::sech(10 * seq(-2, 2, by = 0.01))^2)
+
+growth_decrese_nod <- growth_decrese_nod_1 |>
   tidyr::pivot_longer(cols = contains("rate"),
                       names_pattern = "(.*)_rate",  
                       names_to = "type",
@@ -212,6 +237,46 @@ growth_decrese_nod <- data.table(x = seq(-2, 2, by = 0.01),
                          label = c("Decrease", "Growth")))
 
 ggsave("../../figures/growth_decrese_nod.png",
+       p,
+       height = 3,
+       width = 5,
+       bg = "white")
+
+(p <- ggplot(growth_decrese_nod_1) +
+    geom_hline(aes(yintercept = 0), linetype = "dotted") +
+    geom_line(aes(x, growth_rate - decrease_rate), linewidth = 1) +
+    geom_line(aes(x, jacobian), linewidth = 1, color = "blue") +
+    annotate(geom = "line", 
+             x = seq(-1.2, -0.8, by = 0.1),
+             y = -seq(-1.2, -0.8, by = 0.1) - 1, 
+             linewidth = 2,
+             color = "red") +
+    annotate(geom = "line", 
+             x = seq(-0.2, 0.2, by = 0.1),
+             y = 10 * seq(-0.2, 0.2, by = 0.1), 
+             linewidth = 2,
+             color = "red") +
+    annotate(geom = "line", 
+             x = seq(0.8, 1.2, by = 0.1),
+             y = -seq(0.8, 1.2, by = 0.1) + 1, 
+             linewidth = 2,
+             color = "red") +
+    annotate(geom = "point",
+             x = -1, y = 0,
+             size = 4, color = "magenta")  +
+    annotate(geom = "point",
+             x = 1, y = 0,
+             size = 4, color = "magenta") +
+    annotate(geom = "point",
+             x = 0, y = 0,
+             size = 2, shape = 21, stroke = 2,
+             color = "magenta", fill = "white")  +
+    ylab("Rate") +
+    xlim(c(-1.5, 1.5)) +
+    scale_color_discrete(name = "Type",
+                         label = c("Decrease", "Growth")))
+
+ggsave("../../figures/jacobian_nod.png",
        p,
        height = 3,
        width = 5,
@@ -241,10 +306,30 @@ for(i in 1:length(parameter_space)) {
                values_to = "value") |>
 ggplot() +
   geom_point(aes(alpha, value), size = 2) +
-  ylab("Equilibrium") +
-  xlab("alpha"))
+  ylab("Equilibria x*") +
+  xlab("Attention u"))
 
 ggsave("../../figures/pitchfork.png",
+       p,
+       height = 3,
+       width = 5,
+       bg = "white")
+
+(p <- (results |>
+         tidyr::pivot_longer(cols = contains("equ"),
+                             names_pattern = "equ_(.*)",  
+                             names_to = "branch",
+                             values_to = "value") |>
+         as.data.table()
+)[, ":="(eigenvalue = -1 + alpha * pracma::sech(alpha * value)^2) # first derivative of f(x) inr espect to x evaluated at equilibria
+  ][, ":="(is_stable = eigenvalue > 0)] |> 
+    ggplot() +
+    geom_path(aes(alpha, value, group = branch, linetype = is_stable), linewidth = 2) +
+    guides(linetype = "none") +
+    ylab("Equilibria x*") +
+    xlab("Attention u"))
+
+ggsave("../../figures/pitchfork2.png",
        p,
        height = 3,
        width = 5,
@@ -339,7 +424,7 @@ for(alpha in c(0.5, 1.5)) {
 (p <- ggplot(out_nod) +
     geom_line(aes(time, y, color = start, group = start), linewidth = 1) +
     scale_color_viridis_c(option = "mako") +
-    ylab("Decision") +
+    ylab("Decision x") +
     facet_wrap(~alpha) +
     guides(color = "none") +
     xlab("Time"))
@@ -592,10 +677,11 @@ unfolding <- (read.csv("../../outputs/unfolding.csv",
                 as.data.table()
 )[, ":="(eigenvalues = p + 2 * a * x - 3 * x^2)
 ][, ":="(is_stable = eigenvalues < 0)
-# identify branch id by takeing advantage of the fact that the stability switches every time a nullcline is crossed (from large x to small x) and the same x can be equilibria for multiple branches only when the branche changes stability
+# identify branch id by taking advantage of the fact that the stability switches every time a nullcline is crossed (from large x to small x) and the same x can be equilibria for multiple branches only when the branch changes stability
 ][order(a, b, x, is_stable)
   ][, id := rleid(is_stable),
-    list(a, b)]
+    list(a, b)
+    ][, b := factor(b, levels = sort(unique(b), decreasing = TRUE))]
 
 (p <- ggplot(unfolding) +
   geom_path(aes(p, x, group = id, 
@@ -614,7 +700,161 @@ ggsave("../../figures/unfolding.png",
        width = 9,
        bg = "white")
 
+unfolding <- (read.csv("../../outputs/unfolding.csv",
+                       header = T) |>
+                as.data.table()
+)[, ":="(eigenvalues = p + 2 * a * x - 3 * x^2)
+][, ":="(is_stable = eigenvalues < 0)
+][order(a, b, p, is_stable, x)
+][, diff := p - shift(p), 
+  by = list(a, b)
+][, is_change := diff != shift(diff) & is_stable != shift(is_stable)]
 
+unfolding_sub <- (read.csv("../../outputs/subcritical_unfolding.csv",
+                       header = T) |>
+                as.data.table()
+)[, ":="(eigenvalues = p + 2 * a * x + 3 * x^2)
+][, ":="(is_stable = eigenvalues < 0)
+  # identify branch id by taking advantage of the fact that the stability switches every time a nullcline is crossed (from large x to small x) and the same x can be equilibria for multiple branches only when the branch changes stability
+][order(a, b, x, is_stable)
+][, id := rleid(is_stable),
+  list(a, b)
+][, b := factor(b, levels = sort(unique(b), decreasing = TRUE))]
+
+(p <- ggplot(unfolding_sub) +
+    geom_path(aes(p, x, group = id, 
+                  linetype = is_stable),
+              linewidth = 1) +
+    xlab("Parameter p") +
+    ylab("Equilibria x*") +
+    scale_linetype_manual(values = c("dashed", "solid")) +
+    guides(linetype = "none") +
+    facet_grid(cols = vars(a),
+               rows = vars(b)))
+
+ggsave("../../figures/subcritical_unfolding.png",
+       p,
+       height = 9,
+       width = 9,
+       bg = "white")
+
+
+(p <- ggplot() +
+  geom_hline(aes(yintercept = 0), linewidth = 2) +
+  geom_line(aes(x = seq(-2.5, 2.5, by = 0.001),
+                y = seq(-2.5, 2.5, by = 0.001)^3/27), 
+            linewidth = 2) +
+  geom_point(data = expand.grid(x = c(-2, 0, 2),
+                                y = c(-0.1, 0, 0.1)),
+             aes(x = x, y = y), color = "red", size = 3) +
+  xlab("a") +
+    ylab("b"))
+
+ggsave("../../figures/unfolding_space.png",
+       p,
+       height = 3,
+       width = 5,
+       bg = "white")
+
+
+## Hysteresis ----
+hysteresis <- data.table(x = seq(-1, 1, length = 10000)
+                         )[, ":="(b = -2 * x + atanh(x))
+                           ][, ":="(eigenvalue = -1 + 2 * pracma::sech(b + 2 * x)^2)
+                             ][, ":="(is_stable = eigenvalue > 0)
+                               ][order(x, is_stable)
+                               ][, ":="(branch_id = rleid(is_stable))]
+
+(p <- ggplot(hysteresis) +
+  geom_path(aes(b, x, group = branch_id, 
+                linetype = is_stable),
+            linewidth = 1) +
+  xlab("Input b") +
+  ylab("Equilibria x*") +
+  scale_linetype_manual(values = c("solid", "dashed")) +
+  guides(linetype = "none"))
+
+ggsave("../../figures/histeresis.png",
+       p,
+       height = 3,
+       width = 5,
+       bg = "white")
+
+parameter_space <- seq(0, 2, l = 1000)
+results_2 <- data.table(alpha = NA_real_, b = NA_real_, equ_1 = NA_real_, equ_2 = NA_real_, equ_3 = NA_real_)
+for(input in c(0, 0.1, 0.2)) {
+  for(i in 1:length(parameter_space)) { 
+    alpha <- parameter_space[i]
+    print(alpha)
+    
+    zeros <- mosaic::findZeros(equilibria_nod(x, 1, alpha, input) ~ x, 
+                               xlim = c(-1.5, 1.5))[[1]]
+    
+    zeros <- c(zeros, rep(NA, 3 - length(zeros)))[1:3]
+    
+    results_2 <- rbind(
+      results_2,
+      data.table(alpha = alpha, b = input,
+                 equ_1 = zeros[1], equ_2 = zeros[2], equ_3 = zeros[3])   ) 
+  }
+}
+
+(p <- (results_2 |>
+    tidyr::pivot_longer(cols = contains("equ_"),
+                        names_pattern = "equ_(.*)",  
+                        names_to = "branch",
+                        values_to = "value") |>
+    as.data.table()
+    )[, ":="(eigenvalue = -1 + alpha * pracma::sech(alpha * value + b)^2) # first derivative of f(x) in respect to x evaluated at equilibria
+    ][, ":="(is_stable = eigenvalue > 0)
+      ][order(value, -is_stable)
+        ][, ":="(branch_id = rleid(is_stable)),
+          by = b] |>
+    ggplot() +
+    geom_path(aes(alpha, value, 
+                  group = interaction(branch_id, b), linetype = is_stable, color = b), size = 2) +
+    guides(linetype = "none") +
+    scale_color_viridis_c(name = "Input b") +
+    ylab("Equilibria x*") +
+    xlab("Attention u"))
+
+ggsave("../../figures/pitchfork_unfolded.png",
+       p,
+       height = 3,
+       width = 5,
+       bg = "white")
+
+# Separation of time-scales ----
+nod_superode <- function(time, state, parms) {
+  with(as.list(c(state, parms)), {
+    dydt <- -delta * y + tanh(u * y + b)
+    dudt <- -u + u0 + k * y^2
+    list(c(dydt, dudt))})
+}
+
+# Solve the ODE
+out_supernod <- deSolve::ode(y = c(y = -1, u = 1.5), 
+                          times = seq(0, 100, by = 0.1), 
+                          parms = c(b = 0.1, k = 1, delta = 1, u0 = 0.75),
+                          func = nod_superode) |>
+               as.data.table()
+
+# Plot and save the result
+(p <- ggplot(out_supernod |>
+               tidyr::pivot_longer(cols = c("y", "u"),
+                                   names_to = "variable",
+                                   values_to = "value")) +
+    geom_line(aes(time, value, color = variable), linewidth = 1) +
+    scale_color_discrete(name = "Variable",
+                         labels = c("Attention u", "Decision x")) +
+    ylab("Value") +
+    xlab("Time"))
+
+ggsave("../../figures/supernod_time_series.png",
+       p,
+       height = 3,
+       width = 5,
+       bg = "white")
 
 
 # Selective ultra-sensitivity ----
